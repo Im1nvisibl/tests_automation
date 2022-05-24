1f77ae9fc7ba916b95e4042494fe6a06b530aba0
Vitaliy Somoniuk <vitaliysomonyuk@gmail.com>
2022-05-24 17:52:12 +0300

    Added clicks on links

--- tests/test_homepage.py
+++ tests/test_homepage.py
@@ -1,3 +1,4 @@
+import time
 import pytest
 from pom.home_page_nav import HomepageNav
 
@@ -10,3 +11,9 @@ class TestHomepage:
         actual_links = home_page_nav.get_nav_links_text()
         expected_links = home_page_nav.NAV_LINK_TEXT
         assert expected_links == actual_links, 'Validating Nav Links text'
+        for indx in range(12):
+            home_page_nav.get_nav_links()[indx].click()
+            home_page_nav.driver.delete_all_cookies()
+            time.sleep(3)
+        home_page_nav.get_nav_link_by_name('Men').click()
+        time.sleep(3)
