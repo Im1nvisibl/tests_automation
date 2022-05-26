e811e855a154ee046d8e6ce7994233e2aab3f6d9
Vitaliy Somoniuk <vitaliysomonyuk@gmail.com>
2022-05-26 13:37:53 +0300

    Added cookie removal functionality

--- tests/test_homepage.py
+++ tests/test_homepage.py
@@ -8,12 +8,17 @@ class TestHomepage:
 
     def test_nav_links(self):
         home_page_nav = HomepageNav(self.driver)
-        actual_links = home_page_nav.get_nav_links_text()
-        expected_links = home_page_nav.NAV_LINK_TEXT
-        assert expected_links == actual_links, 'Validating Nav Links text'
+        # home_page_nav.driver.delete_cookie('ak_bmsc')
+        # cookies = home_page_nav.driver.get_cookies()
+        # cookies_names = [cookie['name'] for cookie in cookies]
+        # print(cookies)
+        # print('------------------------------------------')
+        # print(cookies_names)
         for indx in range(12):
             home_page_nav.get_nav_links()[indx].click()
-            home_page_nav.driver.delete_all_cookies()
-            time.sleep(3)
-        home_page_nav.get_nav_link_by_name('Men').click()
-        time.sleep(3)
+            # home_page_nav.driver.delete_cookie('ak_bmsc')
+            # for cookie_name in cookies_names:
+            #     home_page_nav.driver.delete_cookie('ak_bmsc')
+            #     home_page_nav.driver.refresh()
+            #     home_page_nav.is_visible('tag_name', 'h1', cookie_name)
+            # time.sleep(1.5)
